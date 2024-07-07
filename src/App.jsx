159a7b9947275ee60eb4a2a8a32846d535b82cfd
yocom <you@example.com>
2024-07-07 17:57:24 +0330

    -a

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,38 @@
 import { useState , useRef , useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [sorted , setSorted] = useState(false)
-  useEffect(()=>{
-      fetch("https://api.escuelajs.co/api/v1/products?offset=3&limit=13")
-      .then((res) => res.json())
-      .then((output) => setUsers(output))
-  },[]);
-  const Desc = [...users].sort((a,b) => b.price - a.price)
-  const Asc = [...users].sort((a,b) => a.price - b.price)
-  return(
+  const Input = useRef()
+  const Input2 = useRef()
+  const Input3 = useRef()
 
-    <div>
-      <div className='container-button'>
-        <div>
-      <button onClick={()=> setSorted(false)} >Ace</button>
-      <button onClick={()=> setSorted(true)}>Desc</button>
-      </div>
-      <h1>Products</h1>
-      </div>
-      <div className='container'>
-        {sorted ?
-        Desc.map((usersData) => (
-          <div className='main-div' key={usersData.id}>
-              <img src={usersData.images[0]} alt="Images" />
-              <div className='title-div'>
-              <h3>{usersData.title.slice(0,10)}</h3>
-              <p>{usersData.description.slice(0 , 125)}</p>
-              <span className='price'>{usersData.price}$</span>
-              </div>
-            </div>
-           ))
-          :Asc.map((usersData) => (
-            <div className='main-div' key={usersData.id}>
-                <img src={usersData.images[0]} alt="Images" />
-                <div className='title-div'>
-                <h3>{usersData.title.slice(0,15)}</h3>
-                <span className='price'>{usersData.price}$</span>
-                <p>{usersData.description.slice(0 , 125)}</p>
-                </div>
-              </div>
-             ))}
-      </div>
+ function sendInformation(){
+    fetch('https://dummyjson.com/users/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        firstName: `${Input.current.value}`,
+        lastName: `${Input2.current.value}`,
+        age: `${Input3.current.value}`,
+        /* other user data */
+      })
+    })
+    .then(res => res.json())
+    .then(console.log);
+  }
+
+  return(
+    <>
+    <div className='container'>
+      <label htmlFor="">First Name :</label>
+      <input ref={Input} type="text"/>
+      <label htmlFor="">Last Name :</label>
+      <input ref={Input2} type="text"/>     
+      <label htmlFor="">Age :</label>
+      <input ref={Input3} type="text"/>
+      <button onClick={sendInformation}>send</button>
     </div>
+    </>
 )
   
 }
